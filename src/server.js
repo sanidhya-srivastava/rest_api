2fb5fd025c0ab4733958256c4c68bace0e62cf16
Sanidhya Srivastava <sanidsriva@gmail.com>
2025-08-08 19:47:38 +0530

    compress requests and rate limiter

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 import express from "express";
 import helmet from "helmet";
+import { rateLimit } from 'express-rate-limit'
+import compression from 'compression';
 
 import userRoutes from './user.routes.js';
 import mainRoutes from './main.routes.js';
 
 
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+})
+
 const app = express();
 const port = 3000;
 
+app.use(compression())
+app.use(limiter);
 app.use(express.json());
 app.use(helmet());
 
